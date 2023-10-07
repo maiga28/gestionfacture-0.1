d8f24287113c26f99836e59e08fc80e36daa45da
Sam28 <140894390+maiga28@users.noreply.github.com>
2023-10-07 06:06:58 +0200

    Add files via upload

--- payment/views.py
+++ payment/views.py
@@ -1,38 +1,32 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import render, get_object_or_404, redirect
-from factures.models import Facture,LigneFacture
 from .models import Payment
-from products.models import Product
-from .forms import PaymentForm  # Créez un formulaire pour enregistrer les paiements
-
-from django.shortcuts import render, get_object_or_404, redirect
-#from .models import Payment
 from .forms import PaymentForm
-from products.models import Product
-from factures.models import Facture
+from factures.models import LigneFacture,Facture
+
+from django.shortcuts import get_object_or_404, redirect, render
+from django.db.models import Sum  # la fonction Sum pour effectuer des agrégations
 
 def add_payment(request, pk):
     facture = get_object_or_404(Facture, pk=pk)
+    payments = Payment.objects.filter(factures=facture)  # importé le modèle Payment
+
+    # Calculez la somme totale des montants des paiements associés à la facture
+    somme_total = payments.aggregate(Sum('amount'))['amount__sum'] or 0  # la valeur par défaut 0 si la somme est None
 
     if request.method == 'POST':
         form = PaymentForm(request.POST)
         if form.is_valid():
             payment = form.save(commit=False)
-            payment.facture = facture  # Utilisez "facture" au lieu de "Facture"
-            price = Product.objects.filter(price=price)
-            # Récupérez le prix du produit associé à la facture (vous devez spécifier quel produit vous voulez ici)
-            # Supposons que vous avez un lien entre la facture et le produit via la ligne de facture
-            # Pour simplifier, je suppose que vous prenez le premier produit de la facture
-            ligne_facture = facture.lignefacture_set.first()
+            payment.factures = facture  
+
+            ligne_facture = LigneFacture.objects.filter(facture=facture).first()
             if ligne_facture:
                 product_price = ligne_facture.produit.price
-                payment.amount = product_price  # Définissez le montant du paiement sur le prix du produit
-                
+                payment.amount = product_price  #  le montant du paiement sur le prix du produit
+
             payment.save()
-            return redirect('facture_detail', pk=pk)
+            return redirect('factures:facture_detail', pk=facture.pk)  
     else:
         form = PaymentForm()
 
-    return render(request, 'payment/add_payment.html', {'form': form})
+    return render(request, 'payment/add_payment.html', {'form': form, 'somme_total': somme_total})
