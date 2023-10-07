ec57382857b6ed37c06bd2d1dc751c9e28819e19
Sam28 <140894390+maiga28@users.noreply.github.com>
2023-10-07 06:43:57 +0200

    Delete forms.py

--- forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Client
-
-class ClientForm(forms.ModelForm):
-    class Meta:
-        model = Client
-        fields = ['name', 'adresse', 'numero_telephone', 'adresse_email', 'ville', 'pays','apropos',
-    ]
